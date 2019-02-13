b1a0a8df9c4adbea499e04fa9557e6a151ff1f7c
Jake McGinty <me@jake.su>
2019-02-14 04:23:48 +0900

    embrace fancy generics

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -18,9 +18,51 @@ fn log_err<E>(e: E) where E: std::fmt::Debug {
     eprintln!("{:?}", e)
 }
 
+fn build_game_loop<H, S, D>(sink: S, game_state: Arc<Mutex<GameState>>, mut handler: H) -> impl Future<Item = (), Error = ()>
+    where H: Handler + Send + 'static,
+          S: Sink<SinkItem = ws::Message, SinkError = D>,
+          D: std::fmt::Debug,
+{
+    // Create a stream that produces at our desired interval
+    tokio::timer::Interval::new_interval(Duration::from_millis(100))
+        // Give the user a chance to take a turn
+        .map({
+            let game_state = game_state.clone();
+            move |_| {
+                let game_state = game_state.lock().unwrap();
+                handler.tick(&*game_state)
+            }
+        })
+        // Convert their command to a websocket message
+        .map(move |command| ws::Message::Text(serde_json::to_string(&command).unwrap()))
+        // Satisfy the type gods.
+        .map_err(log_err)
+        // And send the message out.
+        .forward(sink.sink_map_err(log_err))
+        .map(|_| ()) // throw away leftovers from forward
+
+}
+
+fn build_state_updater<S, D>(stream: S, game_state: Arc<Mutex<GameState>>) -> impl Future<Item = (), Error = ()>
+    where S: Stream<Item = ws::Message, Error = D>,
+          D: std::fmt::Debug,
+{
+    stream
+        // We only care about text websocket messages.
+        .filter_map(|message| message.into_text().ok())
+        // We especially only care about proper JSON messages.
+        .filter_map(|message| serde_json::from_str(&message).ok())
+        // Update the our game state to the most recent reported by the server.
+        .for_each(move |message| {
+            *game_state.lock().unwrap() = message;
+            Ok(())
+        })
+        .map_err(log_err)
+}
+
 /// Begin the client-side game loop, using the provided struct that implements `Handler`
 /// to act on behalf of the player.
-pub fn run<H>(mut handler: H) -> Result<(), Error> where H: Handler + Send + 'static {
+pub fn run<H>(handler: H) -> Result<(), Error> where H: Handler + Send + 'static {
     let url = url::Url::parse("ws://127.0.0.1:3000/socket")?;
 
     let game_state = Arc::new(Mutex::new(GameState { counter: 0 }));
@@ -29,38 +71,11 @@ pub fn run<H>(mut handler: H) -> Result<(), Error> where H: Handler + Send + 'st
         // Allow us to build two futures out of this connection - one for send, one for recv.
         let (sink, stream) = websocket.split();
 
-        // Create a stream that produces at our desired interval
-        let command_sender = tokio::timer::Interval::new_interval(Duration::from_millis(100))
-            // Give the user a chance to take a turn
-            .map({
-                let game_state = game_state.clone();
-                move |_| {
-                    let game_state = game_state.lock().unwrap();
-                    handler.tick(&*game_state)
-                }
-            })
-            // Convert their command to a websocket message
-            .map(move |command| ws::Message::Text(serde_json::to_string(&command).unwrap()))
-            // Satisfy the type gods.
-            .map_err(log_err)
-            // And send the message out.
-            .forward(sink.sink_map_err(log_err))
-            .map(|_| ()); // throw away leftovers from forward
-
-        let state_updater = stream
-            // We only care about text websocket messages.
-            .filter_map(|message| message.into_text().ok())
-            // We especially only care about proper JSON messages.
-            .filter_map(|message| serde_json::from_str(&message).ok())
-            // Update the our game state to the most recent reported by the server.
-            .for_each(move |message| {
-                *game_state.lock().unwrap() = message;
-                Ok(())
-            })
-            .map_err(log_err);
+        let game_loop = build_game_loop(sink, game_state.clone(), handler);
+        let state_updater = build_state_updater(stream, game_state);
 
         // Return a future that will finish when either one of the two futures finish.
-        state_updater.select(command_sender)
+        state_updater.select(game_loop)
             .then(|_| Ok(()))
     }).map_err(log_err);
 
