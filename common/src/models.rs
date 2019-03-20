0cad61a8e6af51978e51a8c3316d357d70334b54
Brian Schwind <brianmschwind@gmail.com>
2019-03-20 14:35:13 +0900

    Adjust the player speed

--- common/src/models.rs
+++ common/src/models.rs
@@ -7,7 +7,7 @@ use std::{
 pub const BULLET_RADIUS: f32 = 2.0;
 pub const BULLET_SPEED: f32 = 300.0; // in pixels-per-second
 pub const PLAYER_RADIUS: f32 = 10.0;
-pub const PLAYER_BASE_SPEED: f32 = 20.0;
+pub const PLAYER_BASE_SPEED: f32 = 150.0;
 pub const PLAYER_MIN_SPEED: f32 = -1.0;
 pub const PLAYER_MAX_SPEED: f32 = 1.0;
 
